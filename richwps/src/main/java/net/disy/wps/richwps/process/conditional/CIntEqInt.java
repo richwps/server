90952badaed623753993001658496eb0e944bfe9
romanwoessner <roman.woessner@disy.net>
2014-10-24 10:59:29 +0200

    Added three basic conditional algorithms to compare integers, refs #23

---
+++ richwps/src/main/java/net/disy/wps/richwps/process/conditional/CIntEqInt.java
@@ -0,0 +1,43 @@
+package net.disy.wps.richwps.process.conditional;
+
+import org.n52.wps.algorithm.annotation.Algorithm;
+import org.n52.wps.algorithm.annotation.Execute;
+import org.n52.wps.algorithm.annotation.LiteralDataInput;
+import org.n52.wps.algorithm.annotation.LiteralDataOutput;
+import org.n52.wps.io.data.binding.literal.LiteralBooleanBinding;
+import org.n52.wps.io.data.binding.literal.LiteralIntBinding;
+import org.n52.wps.server.AbstractConditionalAnnotatedAlgorithm;
+
+@Algorithm(version = "1.0.0", title="Integer equals integer", abstrakt="Conditional process which checks if integer a equals integer b")
+public class CIntEqInt extends AbstractConditionalAnnotatedAlgorithm {
+
+	int intA, intB;
+	boolean result;
+	
+	@LiteralDataInput(identifier="intA",title="intA", binding=LiteralIntBinding.class)
+	public void setIntA(int a) {
+		intA = a;
+	}
+	
+	@LiteralDataInput(identifier="intB",title="intB", binding=LiteralIntBinding.class)
+	public void setIntB(int b) {
+		intB = b;
+	}
+	
+	@Override
+	@LiteralDataOutput(identifier="result", title="result", binding=LiteralBooleanBinding.class)
+	public boolean getResult() {
+		return this.result;
+	}
+	
+	@Execute
+	public void run() {
+		if (this.intA == this.intB) {
+			this.result = true;
+		}
+		else {
+			this.result = false;
+		}
+	}
+
+}
